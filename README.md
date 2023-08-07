35a29c4cd93848eb9d35af959492cf6fddf50f36
Danylo Sus <danil.sushko19@gmail.com>
2023-08-07 12:10:52 +0300

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Interactive card details form main
+
+from *www.frontendmentor.io*
+
+- [ ] Desktop Design
+- [ ] Responsive Design
+- [ ] Animations
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "linear-gradient-from": "hsl(249, 99%, 64%)",
+        "linear-gradient-to": "hsl(278, 94%, 30%)",
+        red: "hsl(0, 100%, 66%)",
+
+        white: "hsl(0, 0%, 100%)",
+        "light-grayish-violet": "hsl(270, 3%, 87%)",
+        "dark-grayish-violet": "hsl(279, 6%, 55%)",
+        "very-dark-violet": "hsl(278, 68%, 11%)",
+      },
+    },
+  },
+  plugins: [],
+};
